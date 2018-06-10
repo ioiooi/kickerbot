5945f3acf9adba8af7a4dbf756247aebef85dff9
Eugen Mayer <eugen.mayer2@gmail.com>
2018-06-10 20:10:44 +0200

    aws config

---
+++ database/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    region: 'eu-central-1',
+    endpoint: 'http://localhost:8000'
+  },
+  production: {
+    region: 'eu-central-1',
+    endpoint: 'https://dynamodb.eu-central-1.amazonaws.com',
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
+  }
+};
